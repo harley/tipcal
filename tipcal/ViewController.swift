31c15e9f60a4a1373309ea2b41854722a3512559
Harley Trung <harley@harley.io>
2015-08-10 20:24:57 +0700

    add tap recognizer to dismiss the decimal pad

--- tipcal/ViewController.swift
+++ tipcal/ViewController.swift
@@ -43,6 +43,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
 }
-
